1ce342964bcf25f9fc4bae2cb5aedfcfc28c9104
yabuJunn <juancho04reyes@gmail.com>
2024-08-22 10:42:21 -0500

    Creacion de paginas restantes

--- lab3_web/src/componets/page/page.jsx
+++ lab3_web/src/componets/page/page.jsx
@@ -16,6 +16,27 @@ import b2 from "../../assets/Biblioteca.jpg"
 import c2 from "../../assets/Prado con animales.jpg"
 import d2 from "../../assets/Cielo azul con arcoiris.jpg"
 
+//Elige la imagen que mejor representa tu estilo ideal
+
+import a5 from "../../assets/Vestido elegante con gemas.jpg"
+import b5 from "../../assets/Atuendo de Fiesta.jpg"
+import c5 from "../../assets/Atuendo deportivo.jpg"
+import d5 from "../../assets/Atuendo con mariposas.jpg"
+
+//¿Cuál de estas imágenes te inspira más?}
+
+import a7 from "../../assets/Montaña al amanecer.jpg"
+import b7 from "../../assets/Un huerto.jpg"
+import c7 from "../../assets/Nubes suaves.jpg"
+import d7 from "../../assets/Habitacion de costura.jpg"
+
+//Elige la imagen que te haga sonreír
+
+import a8 from "../../assets/Grupo de amigos riendo.jpg"
+import b8 from "../../assets/Arcoiris despues de la lluvia.jpg"
+import c8 from "../../assets/Conejo en jardin.jpg"
+import d8 from "../../assets/Libros y una taza de cafe.jpg"
+
 export function Page() {
     return <>
         {/* <div className='page' id='portada'>
@@ -42,11 +63,60 @@ export function Page() {
             <Button text="Siguiente Pregunta"></Button>
         </div> */}
 
-        <div className='page' id='pregunta3'>
-            <h1>¿Cómo prefieres pasar un día libre?</h1>
+        {/* <div className='page' id='pregunta3'>
+            <h1>¿Cómo te enfrentas a un desafío difícil?</h1>
             <Progress actualPage="3" totalPages="10"></Progress>
             <ResponseOptions type="text" quant={["Con lógica y un plan bien pensado", "Con valentía y determinación", "Pidiendo ayuda a mis amigos, porque juntos somos más fuertes"]}></ResponseOptions>
             <Button text="Siguiente Pregunta"></Button>
-        </div>
+        </div> */}
+
+        {/* <div className='page' id='pregunta4'>
+            <h1>¿Cuál es tu forma favorita de expresarte?</h1>
+            <Progress actualPage="4" totalPages="10"></Progress>
+            <ResponseOptions type="text" quant={["A través de la moda y el diseño", "Organizando eventos divertidos y memorables", "Cuidando de otros, mostrando compasión y amabilidad"]}></ResponseOptions>
+            <Button text="Siguiente Pregunta"></Button>
+        </div> */}
+
+        {/* <div className='page' id='pregunta5'>
+            <h1>Elige la imagen que mejor representa tu estilo ideal</h1>
+            <Progress actualPage="5" totalPages="10"></Progress>
+            <ResponseOptions type="image" quant={[a2, b2, c2, d2]}></ResponseOptions>
+            <Button text="Siguiente Pregunta"></Button>
+        </div> */}
+
+        {/* <div className='page' id='pregunta6'>
+            <h1>¿Cómo manejas el estrés?</h1>
+            <Progress actualPage="6" totalPages="10"></Progress>
+            <ResponseOptions type="text" quant={["Me organizo y hago una lista de tareas para mantener el control", "Me distraigo con una actividad divertida o creativa", "Me tomo un tiempo para relajarme en la naturaleza"]}></ResponseOptions>
+            <Button text="Siguiente Pregunta"></Button>
+        </div> */}
+
+        {/* <div className='page' id='pregunta7'>
+            <h1>¿Cuál de estas imágenes te inspira más?</h1>
+            <Progress actualPage="7" totalPages="10"></Progress>
+            <ResponseOptions type="image" quant={[a7, b7, c7, d7]}></ResponseOptions>
+            <Button text="Siguiente Pregunta"></Button>
+        </div> */}
+
+        {/* <div className='page' id='pregunta8'>
+            <h1>Elige la imagen que te haga sonreír</h1>
+            <Progress actualPage="8" totalPages="10"></Progress>
+            <ResponseOptions type="image" quant={[a8, b8, c8, d8]}></ResponseOptions>
+            <Button text="Siguiente Pregunta"></Button>
+        </div> */}
+
+        {/* <div className='page' id='pregunta9'>
+            <h1>¿Qué harías si un amigo necesita ayuda urgente?</h1>
+            <Progress actualPage="9" totalPages="10"></Progress>
+            <ResponseOptions type="text" quant={["Iría a ayudarlo, nada es más importante que estar ahí para un amigo", "Le ofrecería un hombro en el que apoyarse y palabras de consuelo", "Buscaría la forma más práctica y rápida de resolver su problema"]}></ResponseOptions>
+            <Button text="Siguiente Pregunta"></Button>
+        </div> */}
+
+        {/* <div className='page' id='pregunta10'>
+            <h1>¿Qué valoras más en una amistad?</h1>
+            <Progress actualPage="10" totalPages="10"></Progress>
+            <ResponseOptions type="text" quant={["Iría a ayudarlo, nada es más importante que estar ahí para un amigo", "Le ofrecería un hombro en el que apoyarse y palabras de consuelo", "Buscaría la forma más práctica y rápida de resolver su problema"]}></ResponseOptions>
+            <Button text="Siguiente Pregunta"></Button>
+        </div> */}
     </>
 }
